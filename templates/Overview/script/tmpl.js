39836dc17bd4e7bd1be48a18b3473445d7414fdb
MD.AL-AMIN <mdalmain67378@gmail.com>
2023-08-09 22:33:18 +0600

    upload

---
+++ templates/Overview/script/tmpl.js
@@ -0,0 +1,20 @@
+/**
+ * JYAML - Template Framework includes YAML for Joomla!
+ *
+ * All rights reserved. The JYAML project is a template to manage and
+ * configure Joomla!-Templates with the YAML XHTML/CSS Framework
+ * - http://www.yaml.de
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * @package     JYAML
+ * @subpackage  Javascript
+ *
+ * @author      Reinhard Hiebl <reinhard@hieblmedia.com>
+ * @copyright   Copyright (C) 2006 - 2016, HieblMedia (Reinhard Hiebl)
+ * @license     http://www.jyaml.de/en/license Creative Commons Attribution 3.0
+ * @link        http://www.jyaml.de
+ */
+
+/* Your Code */
+/* ... */
